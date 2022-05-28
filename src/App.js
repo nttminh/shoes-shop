2bf8ccfa0124735ea81b7b961706c0caca79ab81
nttminh <ng.minh@hotmail.com>
2022-05-28 13:05:56 -0400

    style: 💄 change font size of site header
    BREAKING CHANGE: 🧨 \

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ShoesStore from './components/ShoesStore';
-import { Button, Container } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 import ProductItem from './components/ProductItem';
 import { useState } from 'react';
 
@@ -9,7 +9,9 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <h1 className='site-header' align='center'>Shoes Store</h1>
+        <Typography variant='h3' align='center'>
+          Shoes Shop
+        </Typography>
         <ShoesStore />
       </Container>
     </div>
